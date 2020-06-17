6b86371a1cbe6d044d1d2f3db9e290a8a63629c8
Long <thafnhlong@gmail.com>
2020-06-17 21:20:45 +0700

    Add raw template

---
+++ public/script.js
@@ -0,0 +1,41 @@
+$('.nav-item.more').click((ob)=>{
+  if ( $('.category').hasClass('show') )
+  {
+    $('.nav-item.more>a').html('●●●')
+    $('.category').removeClass('show')
+  }
+  else{
+    $('.nav-item.more>a').html('XXX')
+    $('.category').addClass('show')
+    $('.navbar-toggler').click()
+  }
+})
+$('.category').click(()=>$('.nav-item.more').click())
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    items: 3,
+    loop: true,
+    autoplay: false,
+    nav: false,
+    dots: true,
+    autoplayTimeout: 8000,
+    autoplayHoverPause: false,
+    mouseDrag: true,
+    smartSpeed: 1100,
+    margin: 30,
+    navText: ["<i class='icon icon-arrow-left'></i>", "<i class='icon icon-arrow-right'></i>"],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      }
+    }
+  })
+  
+});
