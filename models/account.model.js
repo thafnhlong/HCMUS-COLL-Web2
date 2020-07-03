33bb8c57c5aad381251e2f9445821eb999c7ddee
T-Phong <bao01664165857@gmail.com>
2020-07-03 16:21:08 +0700

    them danh sach tai khoan

--- models/account.model.js
+++ models/account.model.js
@@ -1,12 +1,13 @@
 const db = require('../utils/db');
 const TBL_account = 'account';
-
+const config = require('../config/default.json')
+const moment = require('moment')
 module.exports = {
     add: function (entity) {
         return db.add(TBL_account, entity);
     },
-    getByPermisson: (type)=>{
-      return db.load(`select * from ${TBL_account} where permisson=${type}`)
+    getByPermisson: (type) => {
+        return db.load(`select * from ${TBL_account} where permisson=${type}`)
     },
     singleByEmail: async function (email) {
         const rows = await db.load(`select * from ${TBL_account} where email = '${email}'`);
@@ -15,7 +16,7 @@ module.exports = {
         return rows[0];
     },
     singleByCode: async function (code) {
-        const now=new Date();
+        const now = new Date();
         const rows = await db.load(`select * from ${TBL_account} where verifycode = '${code}' and expiredcode>=${db.escape(now)}`);
         if (rows.length === 0)
             return null;
@@ -27,5 +28,22 @@ module.exports = {
         }
         delete entity.id
         return db.patch(TBL_account, entity, condition);
+    },
+    loadByPage: (offset) => {
+        return db.load(`select * from ${TBL_account} limit ${config.pagination} offset ${offset}`)
+    },
+    count: () => {
+        return db.load(`select count(*) count from ${TBL_account}`)
+    },
+    extend: (entity) => {
+        const condition = {
+            id: entity.id,
+            expired: moment(entity.expired, "DD-MM-YYYY").add(5, 'days')
+        }
+        delete entity.id
+        return db.patch(TBL_account, entity, condition);
+    },
+    del: (id) => {
+        return db.del(TBL_account, { id })
     },
 }
--- routes/_account.route.js
+++ routes/_account.route.js
@@ -1,14 +1,57 @@
 const express = require('express');
+const accountmd = require('../models/account.model')
 const router = express.Router();
+const config = require('../config/default.json')
 
 router.get('/add', function (req, res) {
-  res.render('vwAccount/_edit');
+  
+  res.render('vwAccount/_add');
 })
-router.get('/list', function (req, res) {
-  res.render('vwAccount/_edit');
+
+
+router.get('/list', async function (req, res) {
+  let page = 1
+  
+  let accCount = await accountmd.count()
+  accCount=accCount[0].count
+  const maxPage = Math.ceil(+accCount / config.pagination)
+  if (req.query.page)
+    page = +req.query.page
+  if (page > maxPage)
+    page = +maxPage
+  else if (page < 1)
+    page = 1 
+  const pv = page > 1
+  const nv = page < maxPage 
+  
+  const accList = await accountmd.loadByPage((page-1)*config.pagination)
+  
+  res.render('vwAccount/_list',{
+    accList,
+    page,
+    pag: pv || nv,
+    prev: {isValid:pv,page: page-1},
+    next: {isValid:nv,page: page+1},
+  });
 })
+
+
 router.get('/edit/:id', function (req, res) {
   res.render('vwAccount/_edit');
 })
 
+router.post('/extend', async function (req, res) {
+  await accountmd.extend(req.body.id)
+  res.redirect('back')
+})
+
+router.post('/extend', async function (req, res) {
+  await accountmd.extend(req.body.id)
+  res.redirect('back')
+})
+
+router.post('/delete', async function (req, res) {
+  await accountmd.del(req.body.id)
+  res.redirect('back')
+})
 module.exports = router
